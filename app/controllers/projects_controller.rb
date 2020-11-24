3ca0f895113778b1ab8249ce75255bcc6a34a81d
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-24 17:35:02 -0500

    End of day save, working on Proposals

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,12 @@
 class ProjectsController < ApplicationController
+
+  before_action :load_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -23,22 +25,24 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
+
     @project.update(project_params)
     redirect_to project_path(@project)
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path
   end
 
   private
+
+  def load_project
+    @project = Project.find(params[:id])
+  end
 
   def project_params
     params.require(:project).permit(:title, :description, measurements: [:name, :dimension])
--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -1,2 +1,51 @@
 class ProposalsController < ApplicationController
-end
+
+    before_action :load_proposal, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @proposals = Proposal.all
+    end
+
+    def show
+    end
+
+    def new
+      @proposal = Proposal.new
+    end
+
+    def create
+      @project = Project.find(params[:id])
+      @proposal = Proposal.new(proposal_params)
+      @proposal.user = current_user
+      if @proposal.save
+        redirect_to proposal_path(@proposal)
+      else
+        render "new"
+      end
+      raise
+    end
+
+    def edit
+    end
+
+    def update
+
+      @proposal.update(proposal_params)
+      redirect_to proposal_path(@proposal)
+    end
+
+    def destroy
+      @proposal.destroy
+      redirect_to proposals_path
+    end
+
+    private
+
+    def load_proposal
+      @proposal = Proposal.find(params[:id])
+    end
+
+    def proposal_params
+      params.require(:proposal).permit(:pitch)
+    end
+  end
