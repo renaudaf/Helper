2fbf7e55da59599eea909c1d03c72587c157289f
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-25 16:22:32 -0500

    Fixed dependent destroy that was blocking seed file from working well

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ApplicationRecord
   has_many :proposals, dependent: :destroy
-  has_many :project_tags
+  has_many :project_tags, dependent: :destroy
   has_many :tags, through: :project_tags
 
   belongs_to :user
-  
+
   validates :title, presence: true
 end
