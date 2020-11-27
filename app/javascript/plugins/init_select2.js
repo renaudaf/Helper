d8c874683126e3d20dc75ce7fe2719c835a93710
Javier Granda <javogroj@gmail.com>
2020-11-27 13:00:51 -0700

    added style to project/new

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -1,11 +1,12 @@
-import $ from 'jquery';
+import $, { removeData } from 'jquery';
 import 'select2';
+
 
 const initSelect2 = () => {
   $('#project_tags').select2({
     placeholder: 'Select an option',
     maximumSelectionLength: 3,
-    theme: "classic"});  
+    theme: "classic"});
   console.log(document.getElementById('project_tag_tag'));
 };
 
