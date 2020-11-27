5839bca086c0c6790c8d8e8917933dc5b53ed171
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-27 14:49:18 -0500

    JS working on chat

--- app/javascript/channels/proposal_chat_channel.js
+++ app/javascript/channels/proposal_chat_channel.js
@@ -4,7 +4,6 @@ const initChatroomCable = () => {
   const messagesContainer = document.querySelector('.message-container');
   if (messagesContainer) {
     const id = messagesContainer.dataset.proposalId;
-
     consumer.subscriptions.create({channel:"ProposalChatChannel", id: id}, {
       received(data) {
         messagesContainer.insertAdjacentHTML('beforeend', data);
