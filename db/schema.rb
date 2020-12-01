377aee0f4446d2bcac1dd4e40a0d596df8c9e1b6
Frederic Lauzier <lauzier.frederic@gmail.com>
2020-11-30 20:25:33 -0500

    Starting to work on the user show page so that messages display properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_181238) do
+ActiveRecord::Schema.define(version: 2020_11_25_212738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,8 +63,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_181238) do
     t.datetime "updated_at", precision: 6, null: false
     t.json "measurements", default: []
     t.string "address"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
