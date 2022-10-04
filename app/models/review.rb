1e65b45654ac45d29b6f45374b59c96ac085eb9f
Renaud Archer-Fortin <71739112+renaudaf@users.noreply.github.com>
2022-10-04 11:00:45 -0400

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :reviewer, :class_name => 'User', :foreign_key => 'reviewer_id'
   belongs_to :helper, :class_name => 'User', :foreign_key => 'helper_id'
-
-  # validates :content, :rating, presence: true
-  # validates :rating, inclusion: { in: 1..5,
-  #   message: "%{value} is not a valid rating" }
-  # validates :rating, numericality: true
 end
